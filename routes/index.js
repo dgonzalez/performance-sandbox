87384eab357faf88e9d7a9ce800cc3a9ff54e9bc
Simone Busoli <simone.busoli@gmail.com>
2020-12-02 14:18:02 +0100

    feat: add opossum and fastq

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,42 @@
 "use strict";
 
-const asyncTimeout = (t) => new Promise((resolve) => setTimeout(resolve, t));
+const util = require("util");
+const { ServiceUnavailable } = require("http-errors");
+const fastq = require("fastq");
+const CircuitBreaker = require("opossum");
+
+const asyncTimeout = (t = 1) =>
+  new Promise((resolve) => setTimeout(resolve, t));
+
+const syncTimeout = (t, cb) => setTimeout(cb, t);
 
 module.exports = async function (fastify, opts) {
-  fastify.get("/:delay", async function (request) {
+  const opossumOptions = {
+    timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
+    errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
+    resetTimeout: 30000, // After 30 seconds, try again.
+  };
+
+  const fastQWorkers = 10;
+
+  const queue = fastq(syncTimeout, fastQWorkers);
+  const pushAsync = util.promisify(queue.push);
+
+  const delayHandler = async function (request) {
     const { delay } = request.params;
 
-    await asyncTimeout(delay);
+    await pushAsync(delay);
 
     return { delay };
+  };
+
+  const breaker = new CircuitBreaker(delayHandler, opossumOptions);
+
+  breaker.on("failure", function (error, _, [, reply]) {
+    reply.send(new ServiceUnavailable(error.message));
   });
+
+  fastify.get("/:delay", breaker.fire.bind(breaker));
 
   fastify.get("/under-pressure", async () => fastify.memoryUsage());
 };
